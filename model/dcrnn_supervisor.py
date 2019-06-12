26e21c4928add86d5fd95990474dcc1a75077db4
Minyus <yusuke.minami86@gmail.com>
2019-06-12 18:38:01 +0800

    Add rmse as a loss function.

--- model/dcrnn_supervisor.py
+++ model/dcrnn_supervisor.py
@@ -11,7 +11,7 @@ import yaml
 
 from lib import utils, metrics
 from lib.AMSGrad import AMSGrad
-from lib.metrics import masked_mae_loss
+from lib.metrics import masked_mae_loss, masked_rmse_loss, masked_mse_loss
 
 from model.dcrnn_model import DCRNNModel
 
@@ -94,7 +94,12 @@ class DCRNNSupervisor(object):
         labels = self._train_model.labels[..., :output_dim]
 
         null_val = 0. if kwargs['model'].get('exclude_zeros_in_metric', True) else np.nan
-        self._loss_fn = masked_mae_loss(scaler, null_val)
+
+        loss_func_dict = {'mae': masked_mae_loss(scaler, null_val),
+                          'rmse': masked_rmse_loss(scaler, null_val),
+                          'mse': masked_mse_loss(scaler, null_val)}
+        self._loss_fn = loss_func_dict.get(kwargs['train'].get('loss_func', 'mae'))
+
         self._train_loss = self._loss_fn(preds=preds, labels=labels)
 
         tvars = tf.trainable_variables()
@@ -154,7 +159,7 @@ class DCRNNSupervisor(object):
                             training=False, writer=None,
                             return_ground_truth=False):
         losses = []
-        maes = []
+        # maes = []
         outputs = []
         ground_truths = []
 
@@ -164,7 +169,7 @@ class DCRNNSupervisor(object):
         loss = self._loss_fn(preds=preds, labels=labels)
         fetches = {
             'loss': loss,
-            'mae': loss, # TODO:
+            # 'mae': loss, # TODO:
             'global_step': tf.train.get_or_create_global_step()
         }
         if training:
@@ -197,7 +202,7 @@ class DCRNNSupervisor(object):
             if return_ground_truth:
                 ground_truths.append(y)
 
-            train_steps_per_epoch = self._data_kwargs.get('train_steps_per_epoch', 1000000)
+            train_steps_per_epoch = self._data_kwargs.get('train_steps_per_epoch', 1)
             target_train_steps = self._data_kwargs.get('target_train_steps', 1000000)
             self._logger.debug('Completed {} step #: {:06d} / {:06d} | global step: {:06d} / {:06d}'.\
                                format('training' if training else 'evaluation',
@@ -210,7 +215,7 @@ class DCRNNSupervisor(object):
 
         results = {
             'loss': np.mean(losses),
-            'mae': np.mean(maes)
+            # 'mae': np.mean(maes)
         }
         if return_output:
             results['outputs'] = outputs
@@ -260,7 +265,8 @@ class DCRNNSupervisor(object):
                                                      self._data['train_loader'].get_iterator(),
                                                      training=True,
                                                      writer=self._writer)
-            train_loss, train_mae = train_results['loss'], train_results['mae']
+            train_loss = train_results['loss']
+            # train_mae = train_results['mae']
             if train_loss > 1e5:
                 self._logger.warning('Gradient explosion detected. Ending...')
                 break
@@ -270,18 +276,27 @@ class DCRNNSupervisor(object):
             val_results = self.run_epoch_generator(sess, self._val_model,
                                                    self._data['val_loader'].get_iterator(),
                                                    training=False)
-            val_loss, val_mae = np.asscalar(val_results['loss']), np.asscalar(val_results['mae'])
+            val_loss = np.asscalar(val_results['loss'])
+            # val_mae = np.asscalar(val_results['mae'])
 
             utils.add_simple_summary(self._writer,
-                                     ['loss/train_loss', 'metric/train_mae', 'loss/val_loss',
-                                      'metric/val_mae'],
-                                     [train_loss, train_mae, val_loss, val_mae],
+                                     ['loss/train_loss',
+                                      # 'metric/train_mae',
+                                      'loss/val_loss',
+                                      # 'metric/val_mae',
+                                      ],
+                                     [train_loss,
+                                      # train_mae,
+                                      val_loss,
+                                      # val_mae,
+                                      ],
                                      global_step=global_step)
             end_time = time.time()
             message = \
-                'Epoch: {}/{}|Global Step: {}|train_mae: {:.4f}|val_mae: {:.4f}|lr:{:.6f}|{:.1f}s'.\
-                format(self._epoch, epochs, global_step, train_mae, val_mae, new_lr,
-                       (end_time - start_time))
+                ('Epoch: {}/{}|Global Step: {}|train_loss: {:8.5f}|val_loss: {:8.5f}|' + \
+                'lr:{:.6f}|{:.0f}s/epoch').\
+                format(self._epoch, epochs, global_step, train_loss, val_loss,
+                       new_lr, (end_time - start_time))
             self._logger.info(message)
             if self._epoch % test_every_n_epochs == test_every_n_epochs - 1:
                 self.evaluate(sess)
@@ -290,13 +305,13 @@ class DCRNNSupervisor(object):
                 if save_model > 0:
                     model_filename = self.save(sess, val_loss)
                 self._logger.info(
-                    'Val loss decrease from %.4f to %.4f, saved to %s' % \
-                    (min_val_loss, val_loss, model_filename))
+                    'Val loss decrease from {:8.5f} to {:8.5f}. The model was saved at: {}'.\
+                    format(min_val_loss, val_loss, model_filename))
                 min_val_loss = val_loss
             else:
                 wait += 1
                 if wait > patience:
-                    self._logger.warning('Early stopping at epoch: %d' % self._epoch)
+                    self._logger.warning('Early stopping at epoch: {}'.format(self._epoch))
                     break
 
             history.append(val_mae)
@@ -317,7 +332,6 @@ class DCRNNSupervisor(object):
                                                 return_output=True,
                                                 training=False,
                                                 return_ground_truth=True)
-
 
         test_loss = test_results['loss']
         utils.add_simple_summary(self._writer, ['loss/test_loss'], [test_loss],
